0298fddbdd17c26d59a16d4602c0b7b50038fa60
Oli Wilkinson <Oli Wilkinson>
2016-06-11 11:12:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,140 @@
+# About #
+
+µTest is an ultra-lightweight, minimalist unit test framework for C++11. It is the sister-framework of [µTest for C99](https://github.com/evolutional/utest).
+
+
+## Compiling ##
+
+µTest is provided as a single-header library. To compile, you simply include the header where you need to and ensure that you provide the implementation by defining `UTEST_CPP_IMPLEMENTATION` in **exactly one** source file before you include the header.
+
+		#define UTEST_CPP_IMPLEMENTATION
+		#include "upptest.h"
+
+There are no external dependencies required, there are no special build processes or requirements.
+
+# Usage #
+
+## Implementing Tests ##
+
+This is a simple example of how to implement a basic test in µTest.
+
+	TEST(ExampleFailingEqualityTest, "Example.Tests")
+	{
+		utest::assert::neq(50, 50);
+	}
+
+Each test is declared with a name ('ExampleFailingEqualityTest') and a category ('Example.Tests').
+
+Tests follow the xUnit pattern, with each test being encapsulated in its own class. When declaring tests, it is recommended you declare them in a `.cpp` file as they will automatically register themselves with the engine.
+
+## Assertion ##
+
+µTest provides several simple assertion template functions for use in tests. Is an assert condition fails, a test will terminate immediately, throwing a `utest::assert_fail_exception` which is handled by the execution engine.
+
+Example:
+
+	utest::assert::eq(50, 50);
+
+The basic functions are:
+
+	assert::eq
+	assert::neq
+	assert::is_true
+	assert::is_false
+	assert::is_null
+	assert::is_not_null
+	assert::fail
+ 
+## Executing Tests ##
+
+Executing the tests can be performed in several ways. The simplest is to run all tests that are registered:
+
+	auto res = utest::runner::run_registered([](const auto& tst){
+		// observer fired after completion
+		// you have full info about the test here
+		// including status, error message, execution time, etc
+	});
+
+µTest features no built-in test result reporters, instead you provide your own observer function that is called after each test has been executed.
+
+An example that runs all test and prints the results to `std::cout` is:
+
+	auto res = utest::runner::run_registered([](const utest::result& tst){
+		std::cout << "'" << tst.info->name << "' executed in "
+			<< tst.duration.count() << "ms with result: ";
+		if (tst.status == utest::status::pass)
+			std::cout << "pass" << std::endl;
+		else
+			std::cout << "failed" << std::endl;
+	});
+
+### Filtering ###
+
+Tests can be executed with a filter predicate which will be passed a `const utest::info const*` for evaluation. 
+
+An example which filters tests in a specific category:
+
+	auto res = utest::runner::run_registered([](auto ti) {
+		return std::string(ti->category) == "Example.Tests"; 
+		},	
+		[](const auto&) {}
+	);
+
+All tests are registered with a global `utest::registry`.  You can retrieve these tests by accessing:
+
+	`const auto& tests = utest::registry::get()->tests();`
+
+You can use this to fill your own stl containers and then execute them with `utest::runner::run()`. The run function has various overrides that accepts collections, begin/end range iterators and filter predicates, allowing you to easily create your own filtering and execution process.
+
+## Fixtures ##
+
+µTest supports fixtures by allowing tests to share a common base class and category. This allows you to implement complex tests that share common logic.
+
+Fixtures are declared as follows:
+
+	TEST_FIXTURE(MyFixture)
+	{
+	public:
+		MyFixture()
+			: expected(100)
+		{		
+		}
+
+		int expected;
+	};
+
+There's nothing special about the macro, it simply declares a class that inherits `utest::test`.
+
+From here, you declare fixture tests as follows:
+
+	TEST_F(PassingEqualityTest, MyFixture, "MyFixtures.Tests")
+	{
+		utest::assert::eq(expected, 100);
+	}
+
+
+## FAQ ##
+
+*Will µTest support feature X from {insert popular library}?*
+
+Highly unlikely. µTest created with minimalism in mind. However, if you find yourself implementing a useful, core feature feel free to contribute a pull request.
+
+*Why should I use µTest over {insert popular library}?*
+
+µTest was born out of a need to embed a tiny test framework in a project and disable it with a preprocessor define. 
+
+*Why public domain?*
+
+See [stb's FAQ](https://github.com/nothings/stb) for a great set of reasons. µTest is completely free for you to use in any way you see fit. Attribution is not expected, but it is appreciated.
+
+*What parts of C++11 does µTest use?*
+
+`std::chrono`, `std::memory`, `std::functional`.
+
+*What's the future for µTest?*
+
+µTest is essentially feature complete. It may be updated with a richer set of assert functionality should the need arise.
+
+## License ##
+
+µTest is free and unencumbered software released into the public domain. See UNLICENSE for details.
---
+++ upptest.h
@@ -0,0 +1,397 @@
+#pragma once
+/*
+utest - The micro unit test framework for C99
+
+Author - Oli Wilkinson (https://github.com/evolutional/upptest)
+
+This file provides both the interface and the implementation.
+
+To provide the implementation for this library, you MUST define
+UTEST_CPP_IMPLEMENTATION in exactly on source file.
+
+	#define UTEST_CPP_IMPLEMENTATION
+	#include "upptest.h"
+
+USAGE
+-----
+
+See README.md for details
+
+
+LICENSE
+-------
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
+*/
+
+#include <chrono>
+#include <exception>
+#include <functional>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace utest
+{
+#define TEST_FIXTURE(name)		class name : public utest::test	
+#define SETUP()			virtual void pre_test() override
+#define TEARDOWN()		virtual void post_test() override
+#define TEST_AUTO_NAME(name)	g_utestautoreg_test_##name
+#define TEST_F(name, fixture, category)	\
+	class name : public fixture	\
+	{	\
+		public:	\
+			name() : fixture() {}	\
+			void execute_test() override;	\
+			static utest::info s_info;	\
+	};	\
+	utest::info name::s_info([]() {return std::make_unique< name >();}, #name, category, __FILE__, __LINE__);	\
+	namespace { utest::auto_registered_test TEST_AUTO_NAME(name)(&name::s_info); } \
+	void name::execute_test()
+
+#define TEST(name, category)	TEST_F(name, utest::test, category)
+
+	class assert_fail_exception
+		: public std::exception
+	{
+	public:
+		explicit assert_fail_exception(const std::string& msg, const char* file_name, const int line_num)
+			: _msg(msg)
+			, _file_name(file_name)
+			, _line_num(line_num)
+		{
+		}
+		const char* what() const override { return _msg.c_str(); }
+		std::string what_str() const { return _msg; }
+		const char* file_name() const { return _file_name; }
+		int line_num() const { return _line_num; }
+	private:
+		std::string _msg;
+		const char* _file_name;
+		const int _line_num;
+	};
+
+	enum class status
+	{
+		not_run,
+		pass,
+		fail
+	};
+	
+	class info;
+
+	struct result final
+	{
+		result()
+			: info(nullptr)
+			, status(status::not_run)
+			, duration()
+			, err_message()
+			, err_file(nullptr)
+			, err_line(0)
+		{}
+
+		void exception(const std::exception& ex)
+		{
+			status = status::fail;
+			err_message = "unhandled exception";
+		}
+
+		void fail(const std::string& msg, const char* file_name, const int line_num)
+		{
+			status = status::fail;
+			err_message = msg;
+			err_file = file_name;
+			err_line = line_num;
+		}
+
+		const info* info;
+		status status;
+		std::chrono::milliseconds duration;
+		std::string err_message;
+		const char* err_file;
+		int err_line;
+	};
+
+	class test;
+	
+	class info
+	{
+	public:
+		typedef std::function<std::unique_ptr<test>()> factory_func;
+		info(const factory_func& f_, const char* name_, const char* category_, const char* file_, const int line_)
+			: f(f_)
+			, name(name_)
+			, category(category_)
+			, file(file_)
+			, line(line_)
+		{}
+
+		factory_func f;
+		const char* name;
+		const char* category;
+		const char* file;
+		const int line;
+	};
+
+	class default_fail_handler
+	{
+	public:
+		static void handle(const std::string& message, const char* file_name = "", const int line_num = 0)
+		{
+			throw assert_fail_exception(message, file_name, line_num);
+		}
+	};
+
+	template<class fail_handler>
+	class basic_assert
+	{
+	public:
+		template<typename T>
+		static void eq(const T& expected, const T& actual, const char* file_name = "", const int line_num = 0)
+		{
+			if (expected == actual)
+			{
+				return;
+			}
+			std::ostringstream err;
+			err << "Expected [" << expected << "] saw [" << actual << "]";
+			fail(err.str(), file_name, line_num);
+		}
+
+		template<typename T>
+		static void neq(const T& expected, const T& actual, const char* file_name = "", const int line_num = 0)
+		{
+			if (expected != actual)
+			{
+				return;
+			}
+			std::ostringstream err;
+			err << "Expected [" << expected << "] saw [" << actual << "]";
+			fail(err.str(), file_name, line_num);
+		}
+
+		static void is_true(const bool condition, const char* file_name = "", const int line_num = 0)
+		{
+			if (condition)
+			{
+				return;
+			}
+			fail("Expected [true] saw [false]", file_name, line_num);
+		}
+
+		static void is_false(const bool condition, const char* file_name = "", const int line_num = 0)
+		{
+			if (!condition)
+			{
+				return;
+			}
+			fail("Expected [false] saw [true]", file_name, line_num);
+		}
+
+		template<typename T>
+		static void is_null(const T* ptr, const char* file_name = "", const int line_num = 0)
+		{
+			if (ptr == nullptr)
+			{
+				return;
+			}
+			fail("Expected nullptr", file_name, line_num);
+		}
+
+		template<typename T>
+		static void is_not_null(const T* ptr, const char* file_name = "", const int line_num = 0)
+		{
+			if (ptr != nullptr)
+			{
+				return;
+			}
+			fail("Expected non-nullptr", file_name, line_num);
+		}
+
+		static void fail(const std::string& message, const char* file_name = "", const int line_num = 0)
+		{
+			fail_handler::handle(message, file_name, line_num);
+		}
+	};
+
+	typedef basic_assert<default_fail_handler> assert;
+
+	class test
+	{
+	public:
+		virtual ~test() {}
+
+		status execute(result& res)
+		{
+			auto test_start_time = std::chrono::steady_clock::now();
+			try
+			{
+				pre_test();
+				execute_test();
+				res.status = status::pass;
+			}
+			catch (const assert_fail_exception& ex)
+			{
+				res.fail(ex.what_str(), ex.file_name(), ex.line_num());
+			}
+			catch (const std::exception& ex)
+			{
+				res.exception(ex);
+			}
+			post_test();			
+			auto test_end_time = std::chrono::steady_clock::now();
+			auto test_duration = std::chrono::duration_cast<std::chrono::milliseconds>(test_end_time - test_start_time);
+			res.duration = test_duration;
+			return res.status;
+		}
+
+	protected:
+		test(){}
+		virtual void pre_test() {}
+		virtual void post_test() {}
+	private:
+		virtual void execute_test() = 0;
+	};	
+
+	class registry
+	{
+	public:
+		typedef std::vector<const info*> container_type;
+
+		static registry& get();
+		void add(const info* test)
+		{
+			_tests.push_back(test);
+		}
+		container_type& tests() { return _tests; }
+	private:
+		registry();
+		container_type _tests;
+
+	private:
+		static std::unique_ptr<registry> _instance;
+	};
+
+	class auto_registered_test
+	{
+	public:
+		explicit auto_registered_test(const info* info) : _info(info)
+		{
+			registry::get().add(info);
+		}
+		const info* _info;
+	};
+
+	class runner
+	{
+	public:
+
+		static status run(const info const* ti, result& out_res)
+		{
+			auto tst = ti->f();
+			out_res.info = ti;
+			return tst->execute(out_res);
+		}
+
+		template<typename iterator_type, class execution_observer>
+		static status run(iterator_type itr_begin, iterator_type itr_end,
+			const execution_observer& observer)
+		{
+			return run(itr_begin, itr_end, [](auto) { return true; }, observer);
+		}
+
+		template<typename iterator_type, class binary_predicate, class execution_observer>
+		static status run(iterator_type itr_begin, iterator_type itr_end, const binary_predicate& filter,
+			const execution_observer& observer)
+		{
+			size_t pass(0), test_count(0);
+			for (auto itr = itr_begin; itr != itr_end; ++itr)
+			{
+				const auto const* ti = *itr;
+				if (filter(ti))
+				{
+					result res;
+					auto r = run(ti, res);
+					if (r == status::pass)
+					{
+						++pass;
+					}
+					++test_count;
+					observer(res);
+				}
+			}
+			return pass == test_count ? status::pass : status::fail;
+		}
+
+		template<typename container_type, class execution_observer>
+		static status run(const container_type& tests, const execution_observer& observer)
+		{
+			return run(tests.begin(), tests.end(), observer);
+		}
+
+		template<typename container_type, class binary_predicate, class execution_observer>
+		static status run(const container_type& tests, const binary_predicate& filter, const execution_observer& observer)
+		{
+			return run(tests.begin(), tests.end(), filter, observer);
+		}
+
+		template<class execution_observer>
+		static status run_registered(const execution_observer& observer)
+		{
+			return run(registry::get().tests(), [](auto) { return true; }, observer);
+		}
+
+		template<class binary_predicate, class execution_observer>
+		static status run_registered(const binary_predicate& filter, const execution_observer& observer)
+		{
+			return run(registry::get().tests(), filter, observer);
+		}
+	};
+
+#ifdef UTEST_CPP_IMPLEMENTATION
+
+	std::unique_ptr<registry> registry::_instance;
+
+	registry& registry::get()
+	{
+		if(!_instance)
+		{
+			_instance = std::unique_ptr<registry>(new registry());
+		}
+		return *_instance;
+	}
+
+	registry::registry()
+		: _tests()
+	{
+	}
+
+#endif	// UTEST_CPP_IMPLEMENTATION
+
+}
+
